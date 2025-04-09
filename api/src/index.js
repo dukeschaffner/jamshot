c6ab34287777b95ed6dd09de0593c13cd1a8d46b
sunhertzmusic <sunhertzmusic@gmail.com>
2025-04-08 23:34:02 -0500

    update api port

--- api/src/index.js
+++ api/src/index.js
@@ -10,7 +10,7 @@ const paymentRoutes = require('./routes/payments');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors({
   origin: (origin, callback) => {
