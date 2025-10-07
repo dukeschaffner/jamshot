2e4998d3d7cc11ca4c6a0fdafe42ca5574a523c9
sunhertzmusic <sunhertzmusic@gmail.com>
2025-10-07 11:47:33 -0500

    revert cors and serverless express tests

--- api/lambda/index.js
+++ api/lambda/index.js
@@ -3,9 +3,7 @@ const app = require('./src/index');
 
 // Create serverless express instance with proper binary handling
 const serverlessExpressInstance = serverlessExpress({
-  app,
-  binaryMimeTypes: ['application/json', 'application/x-www-form-urlencoded'],
-  resolutionMode: 'CALLBACK' // Use callback resolution for better stream handling
+  app
 });
 
 // Lambda handler function
