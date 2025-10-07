2b1516d9eec1b524324711e5d15f271ac46e5593
sunhertzmusic <sunhertzmusic@gmail.com>
2025-10-07 12:36:21 -0500

    api request logging

--- api/lambda/index.js
+++ api/lambda/index.js
@@ -3,9 +3,7 @@ const app = require('./src/index');
 
 // Create serverless express instance with proper configuration
 const serverlessExpressInstance = serverlessExpress({
-  app,
-  // Configure binary types to handle properly
-  binaryMimeTypes: ['application/octet-stream', 'image/*']
+  app
 });
 
 // Lambda handler function
@@ -13,13 +11,7 @@ exports.handler = async (event, context) => {
   // Set callbackWaitsForEmptyEventLoop to false to prevent Lambda from waiting
   // for the event loop to be empty before returning
   context.callbackWaitsForEmptyEventLoop = false;
-
-  // Log key event details for debugging (remove in production)
-  console.log('Event method:', event.httpMethod);
-  console.log('Event path:', event.path);
-  console.log('Content-Type:', event.headers ? event.headers['content-type'] : 'none');
-  console.log('Body present:', !!event.body);
-  console.log('Body type:', typeof event.body);
-
+  console.log('Event body type:', typeof event.body);
+  console.log('Event isBase64Encoded:', event.isBase64Encoded);
   return serverlessExpressInstance(event, context);
 };
