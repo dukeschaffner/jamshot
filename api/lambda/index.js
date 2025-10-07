dca4fa9185dcf3f50ade1579b393ad6be8b9be27
sunhertzmusic <sunhertzmusic@gmail.com>
2025-10-06 21:32:22 -0500

    api lambda test fix

--- api/lambda/index.js
+++ api/lambda/index.js
@@ -1,8 +1,12 @@
 const serverlessExpress = require('@codegenie/serverless-express');
 const app = require('./src/index');
 
-// Create serverless express instance with configuration
-const serverlessExpressInstance = serverlessExpress({ app });
+// Create serverless express instance with proper binary handling
+const serverlessExpressInstance = serverlessExpress({
+  app,
+  binaryMimeTypes: ['application/json', 'application/x-www-form-urlencoded'],
+  resolutionMode: 'CALLBACK' // Use callback resolution for better stream handling
+});
 
 // Lambda handler function
 exports.handler = async (event, context) => {
