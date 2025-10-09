31014f8f7f4883b5242165e9315a471ae13cba5e
sunhertzmusic <sunhertzmusic@gmail.com>
2025-10-09 16:37:43 -0500

    Disable body parsing for Stripe webhook route in middleware to ensure raw body access for signature verification.

--- api/lambda/src/index.js
+++ api/lambda/src/index.js
@@ -51,7 +51,7 @@ app.use(globalLimiter);
 app.use(speedLimiter);
 
 
-app.use('/api/payments/webhook', express.raw({ type: 'application/json' }));
+//app.use('/api/payments/webhook', express.raw({ type: 'application/json' }));
 
 if (process.env.NODE_ENV === 'dev') {
   app.use(express.json({ limit: '50mb' }));
