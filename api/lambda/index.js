3cab5f200c4633678dfa5ddfb680029eb27dc95a
sunhertzmusic <sunhertzmusic@gmail.com>
2025-10-09 16:31:45 -0500

    logging

--- api/lambda/index.js
+++ api/lambda/index.js
@@ -14,9 +14,11 @@ exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   console.log('Event body type:', typeof event.body);
   console.log('Event isBase64Encoded:', event.isBase64Encoded);
+  console.log('Event body:', event.body);
 
   if (event.body && event.isBase64Encoded) {
     event.body = Buffer.from(event.body, 'base64');
+    console.log('Event body decoded:', event.body);
   }
 
   console.log('Event url:', event.url);
