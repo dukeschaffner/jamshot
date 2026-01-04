e35a7ff15a9229e79058344e822303f166124e62
sunhertzmusic <sunhertzmusic@gmail.com>
2026-01-04 11:13:16 -0600

    move user edit to its own page

---
+++ ui/src/components/NotificationsSettings.js
@@ -0,0 +1,14 @@
+'use client';
+import styles from './NotificationsSettings.module.css';
+
+export default function NotificationsSettings() {
+  return (
+    <div className={styles.notificationsSettings}>
+      <h1 className={styles.title}>Notifications</h1>
+      <p className={styles.placeholder}>
+        Notification settings will be available here soon.
+      </p>
+    </div>
+  );
+}
+
