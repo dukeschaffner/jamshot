f8541e9b947c36ed06b8702dc8b042bf7f61e299
sunhertzmusic <sunhertzmusic@gmail.com>
2026-01-21 10:10:29 -0600

    api build test fix

--- api/lambda/better-auth_migrations/2026-01-07T21-51-44.625Z.sql
+++
@@ -1,7 +0,0 @@
-create table "session" ("id" text not null primary key, "expiresAt" timestamptz not null, "token" text not null unique, "createdAt" timestamptz default CURRENT_TIMESTAMP not null, "updatedAt" timestamptz not null, "ipAddress" text, "userAgent" text, "userId" text not null references "users" ("id") on delete cascade);
-
-create table "account" ("id" text not null primary key, "accountId" text not null, "providerId" text not null, "userId" text not null references "users" ("id") on delete cascade, "accessToken" text, "refreshToken" text, "idToken" text, "accessTokenExpiresAt" timestamptz, "refreshTokenExpiresAt" timestamptz, "scope" text, "password" text, "createdAt" timestamptz default CURRENT_TIMESTAMP not null, "updatedAt" timestamptz not null);
-
-create index "session_userId_idx" on "session" ("userId");
-
-create index "account_userId_idx" on "account" ("userId");
