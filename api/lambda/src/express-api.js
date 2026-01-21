a83b2ec016df00659faa0effe1d2aa612c489506
sunhertzmusic <sunhertzmusic@gmail.com>
2026-01-21 15:39:00 -0600

    api build test fix

--- api/lambda/src/express-api.js
+++ api/lambda/src/express-api.js
@@ -1,5 +1,3 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
 
 import userRoutes from './routes/users.js';
 import trackRoutes from './routes/tracks.js';
