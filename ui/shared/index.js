c6beded13cbc98e8d2d3cc4209f5392efa52a5b8
sunhertzmusic <sunhertzmusic@gmail.com>
2026-01-22 15:18:06 -0600

    ui cleanup

--- ui/shared/index.js
+++ ui/shared/index.js
@@ -1,16 +1,11 @@
 // Auto-generated ES6 exports for UI
 
 import { createApiClient, createApiMethods } from './api/index.js';
-import { AUDIO_CONSTANTS, SUBSCRIPTION_TIERS, FEED_TYPES, PRIVACY_TYPES } from './types/index.js';
 import { formatDuration, formatDate, formatDateTime, formatCompetitionDateRange, formatTimeAgo, formatPlayCount, getLikeCountString, getCommentCountString, formatFollowerCount, formatFileSize, formatUsername, truncateText, capitalizeWords, posToTime, timeToPos, getCountryName, getCountryFlag } from './utils/formatting.js';
 import { validateDateOfBirth } from './utils/validation.js';
 
 export { createApiClient };
 export { createApiMethods };
-export { AUDIO_CONSTANTS };
-export { SUBSCRIPTION_TIERS };
-export { FEED_TYPES };
-export { PRIVACY_TYPES };
 export { formatDuration };
 export { formatDate };
 export { formatDateTime };
