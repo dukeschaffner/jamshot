1db67dfb9d79d6f0d90d556879fc4c904f93edc0
sunhertzmusic <sunhertzmusic@gmail.com>
2026-01-22 08:09:10 -0600

    delete unused code

--- ui-oauth-tester/src/lib/auth.js
+++
@@ -1,9 +0,0 @@
-import { createAuthClient } from 'better-auth/react';
-
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
-
-export const authClient = createAuthClient({
-  baseURL: API_URL,
-  basePath: '/api/auth',
-});
-
