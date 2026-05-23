c8a6ddd5301367ab67fee6cacf268dbfd099b155
sunhertzmusic <sunhertzmusic@gmail.com>
2026-05-22 23:03:46 -0500

    build test fix

--- cms/next.config.mjs
+++ cms/next.config.mjs
@@ -11,6 +11,22 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Payload pulls in pino; Turbopack externalizes it but thread-stream was missing from Lambda traces.
+  serverExternalPackages: ['pino-pretty', 'pino-abstract-transport'],
+  outputFileTracingIncludes: {
+    '**/*': [
+      './node_modules/thread-stream/index.js',
+      './node_modules/thread-stream/lib/**/*',
+      './node_modules/thread-stream/package.json',
+      './node_modules/pino/**/*',
+      './node_modules/pino-abstract-transport/**/*',
+      './node_modules/@pinojs/redact/**/*',
+      './node_modules/payload/node_modules/thread-stream/index.js',
+      './node_modules/payload/node_modules/thread-stream/lib/**/*',
+      './node_modules/payload/node_modules/thread-stream/package.json',
+      './node_modules/payload/node_modules/pino/**/*',
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
