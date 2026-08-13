b7f3e4dea5a60f4886bd1a4c25f7a0153d88c980
sunhertzmusic <sunhertzmusic@gmail.com>
2026-08-13 14:42:12 -0500

    update plugin config

--- plugin/Source/Config.h
+++ plugin/Source/Config.h
@@ -22,8 +22,8 @@ public:
         /** Base URL for API requests */
         static juce::String getBaseUrl()
         {
-            // return "https://api.sterio.fm/api";
-            return "http://localhost:5001/api";
+            return "https://api.sterio.fm/api";
+            // return "http://localhost:5001/api";
         }
     };
 
@@ -34,8 +34,8 @@ public:
         /** Base URL for web app */
         static juce::String getBaseUrl()
         {
-            // return "https://sterio.fm";
-            return "http://localhost:3000";
+            return "https://sterio.fm";
+            // return "http://localhost:3000";
         }
     };
 
